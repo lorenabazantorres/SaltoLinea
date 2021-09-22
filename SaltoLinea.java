a87f94713e2f277771460d9a34d73f3d2c81dbf1
lorenabazantorres <lorenabazantorres@gmail.com>
2021-09-22 12:22:12 +0200

    Create SaltoLinea.java

---
+++ SaltoLinea.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package saltolinea;
+
+/**
+ *
+ * @author Alumno mañana
+ */
+public class SaltoLinea {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String var = "Variable";
+        System.out.println("PRUEBA1\n");
+        System.out.println(var + "\t" + var);
+        System.out.println("PRUEBA2\n");
+        System.out.println(var + "\n" + var);
+        System.out.println("PRUEBA3\n");
+        System.out.println(var + "\b" + var);
+    }
+
+}
